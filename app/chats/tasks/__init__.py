a5f39be72a4fef38ca5062550c84373d1e16165d
Forgot-0 <mr.aisamutdin@mail.ru>
2026-05-15 22:06:04 +0300

    AttachmentProccessTask

--- app/chats/tasks/__init__.py
+++ app/chats/tasks/__init__.py
@@ -1,17 +1,7 @@
 from taskiq import AsyncBroker
 
-# from app.chats.tasks.read import FlushReadReceiptsTask
-# from app.chats.tasks.success_attachment import AttachmentProccessTask
+from app.chats.tasks.success_attachment import AttachmentProccessTask
 
 
 def register_chat_tasks(broker: AsyncBroker) -> None:
-    # broker.register_task(
-    #     FlushReadReceiptsTask.run,
-    #     FlushReadReceiptsTask.get_name(),
-    #     labels={
-    #         "schedule": [{"cron": "*/5 * * * *"}]
-    #     },
-    # )
-
-    # broker.register_task(AttachmentProccessTask.run, AttachmentProccessTask.get_name())
-    ...
+    broker.register_task(AttachmentProccessTask.run, AttachmentProccessTask.get_name())
