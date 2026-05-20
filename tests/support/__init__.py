eec8a429e47bd449055ef959fee2a6b0881fe3ab
Forgot-0 <mr.aisamutdin@mail.ru>
2026-05-20 17:11:55 +0300

    digets

--- tests/support/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Shared test utilities (URLs, JWT helpers, handler wiring)."""
-
-from tests.support.http import api_path
-from tests.support.jwt import jwt_from_user
-
-__all__ = ["api_path", "jwt_from_user"]
